cb9fb385793a0d4bc4a17d016050993d19ba3eb0
Nikei <Nikei@DESKTOP-NH3O0C1>
2024-12-18 13:53:35 +1000

    Добавьте файлы проекта.

---
+++ КТ № 23 Разработка программы с использованием класса/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КТ___23_Разработка_программы_с_использованием_класса
+{
+    public class Example
+    {
+        public static void Main(string[] args)
+        {
+            MyClass myObject = new MyClass(10, 5);// Создание знач.
+            myObject.PrintValues();//Вывод знач.
+            Console.WriteLine($"Сумма: {myObject.Sum()}");
+            Console.WriteLine($"Наибольшее значение: {myObject.Max()}");
+
+
+
+            myObject.ChangeValues(20, 15);//Изминение знач.
+            myObject.PrintValues();//Вывод нов. знач.
+            Console.WriteLine($"Сумма: {myObject.Sum()}"); 
+            Console.WriteLine($"Наибольшее значение: {myObject.Max()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ КТ № 23 Разработка программы с использованием класса/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КТ___23_Разработка_программы_с_использованием_класса
+{
+    public class MyClass
+    {
+        public int Value1;
+        public int Value2; 
+
+        // Конструктор
+        public MyClass(int value1, int value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+
+        //Вывод значений
+        public void PrintValues()
+        {
+            Console.WriteLine($"Значение 1: {Value1}, Значение 2: {Value2}");
+        }
+
+        //Изменение переменных
+        public void ChangeValues(int newValue1, int newValue2)
+        {
+            Value1 = newValue1;
+            Value2 = newValue2;
+        }
+
+        public int Sum()
+        {
+            return Value1 + Value2;
+        }
+
+        public int Max()
+        {
+            return Math.Max(Value1, Value2);
+        }
+    }
+}
